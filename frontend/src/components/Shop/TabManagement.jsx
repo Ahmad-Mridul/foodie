02e168313592760bd00fdfd297142f76b430f3b1
Ahmad-Mridul <mridulahmed262@gmail.com>
2025-08-01 21:18:22 +0600

    minor changes in design

--- frontend/src/components/Shop/TabManagement.jsx
+++ frontend/src/components/Shop/TabManagement.jsx
@@ -3,42 +3,48 @@ import ProductCard from "../shared/ProductCard";
 
 const TabManagement = () => {
     const [value, setValue] = useState(null);
-    
+
     return (
         <div className="space-y-10 mx-auto text-center">
             <div className="space-x-5">
                 <button
-                    className="btn btn-primary uppercase"
+                    className="btn hover:shadow-xl hover:bg-transparent hover:border-[#B58130] uppercase bg-[#B58130]"
+                    onClick={() => setValue("")}
+                >
+                    all
+                </button>
+                <button
+                    className="btn hover:shadow-xl hover:bg-transparent hover:border-[#B58130] uppercase bg-[#B58130]"
                     onClick={() => setValue("salad")}
                 >
                     salads
                 </button>
                 <button
-                    className="btn btn-primary uppercase"
+                    className="btn hover:shadow-xl hover:bg-transparent hover:border-[#B58130] uppercase bg-[#B58130]"
                     onClick={() => setValue("pizza")}
                 >
                     pizzas
                 </button>
                 <button
-                    className="btn btn-primary uppercase"
+                    className="btn hover:shadow-xl hover:bg-transparent hover:border-[#B58130] uppercase bg-[#B58130]"
                     onClick={() => setValue("soup")}
                 >
                     soups
                 </button>
                 <button
-                    className="btn btn-primary uppercase"
+                    className="btn hover:shadow-xl hover:bg-transparent hover:border-[#B58130] uppercase bg-[#B58130]"
                     onClick={() => setValue("drinks")}
                 >
                     drinks
                 </button>
                 <button
-                    className="btn btn-primary uppercase"
+                    className="btn hover:shadow-xl hover:bg-transparent hover:border-[#B58130] uppercase bg-[#B58130]"
                     onClick={() => setValue("dessert")}
                 >
                     desserts
                 </button>
             </div>
-            <ProductCard category={value}/>
+            <ProductCard category={value} />
         </div>
     );
 };
